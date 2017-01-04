826329cd5a289488eed4e8e6f54cdf882a9208b7
DianeLisaMere <diane.douglas1@gmail.com>
2017-01-04 15:45:24 -0500

    add firebase npm package and initialize in app component constructor using import rather than script tags in index.html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp, database } from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  constructor() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAwuKl7qjXwEFV4e_XdCLY8goNChIMgc1w",
+      authDomain: "tutorial-b967f.firebaseapp.com",
+      databaseURL: "https://tutorial-b967f.firebaseio.com",
+      storageBucket: "tutorial-b967f.appspot.com",
+      messagingSenderId: "979501840072"
+    };
+    initializeApp(config);
+
+    var root = database().ref('users');
+    root.on('value', function(snap){
+      var users = snap.val();
+      var key = snap.key;
+      // console.log(users[0].name);
+      users.forEach(function(user){
+        console.log(user.name);
+      })
+    })
+  }
 }
