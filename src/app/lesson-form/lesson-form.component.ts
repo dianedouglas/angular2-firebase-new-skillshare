7002be34fd1347c9268d77ae7c2b661de427e793
Gibberish314 <a.moosen@gmail.com>
2017-01-20 11:57:04 -0500

    add form with required fields.

--- src/app/lesson-form/lesson-form.component.ts
+++ src/app/lesson-form/lesson-form.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
-  selector: 'app-lesson-form',
+  selector: 'lesson-form',
   templateUrl: './lesson-form.component.html',
   styleUrls: ['./lesson-form.component.css']
 })
 export class LessonFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      description: ['placeholder', Validators.required],
+      url: ['', Validators.required],
+      videoUrl: ['', Validators.required],
+      tags: ['', Validators.required],
+      longDescription: ['']
+    });
+  }
+
+  isErrorVisible(field: string, error:string) {
+    return this.form.controls[field].dirty //has been touched
+      && this.form.controls[field].errors //if form is associated with the field
+      && this.form.controls[field].errors[error]; //if the error includes the given error.
   }
 
 }
